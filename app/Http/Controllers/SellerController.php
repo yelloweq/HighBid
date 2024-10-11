02b7a8e9d42f30efeb4a1b7b643c90608697f4df
yelloweq <iwo.grzes237@gmail.com>
2024-10-11 14:29:55 +0100

    TODOs & moving logic

--- app/Http/Controllers/SellerController.php
+++ app/Http/Controllers/SellerController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Payment\PaymentGateway;
+use App\Payment\StripePayment;
 use App\Services\Stripe\Seller;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -13,57 +15,5 @@ use Stripe\StripeClient;
 
 class SellerController extends Controller
 {
-    public function create(): HtmxResponseClientRedirect
-    {
-        dd("IM HERE");
-        $stripe = new StripeClient(config('stripe.sk'));
 
-        dd($stripe);
-        $user = User::find(Auth::id());
-        if (!is_null($user->stripe_account_id)) {
-            return new HtmxResponseClientRedirect(route('payment.login'));
-        }
-
-        $connect_account = $stripe->accounts->create(['type' => 'express']);
-        $user->update(['stripe_account_id' => $connect_account->id]);
-        $user->save();
-
-        return new HtmxResponseClientRedirect(route('login.express'));
-    }
-
-    public function save(Request $request): HtmxResponseClientRedirect
-    {
-        $this->validate($request, [
-            'code' => 'required',
-            'state' => 'required',
-        ]);
-
-        $session = Session::where('id', $request->state)->first();
-        if (is_null($session)) {
-            return new HtmxResponseClientRedirect(route('auctions'));
-        }
-
-        $data = Seller::create($request->code);
-        User::find($session->user_id)->update([
-            'stripe_connect_id' => $data->stripe_user_id,
-            'stripe_account_id' => $data->stripe_account_id,
-        ]);
-
-        return new HtmxResponseClientRedirect(route('auctions'));
-    }
-
-    public function login(): RedirectResponse
-    {
-        $user = Auth::user();
-        $stripe = new StripeClient(config('stripe.sk'));
-
-        $account_link = $stripe->accountLinks->create([
-            'account' => $user->stripe_account_id,
-            'refresh_url' => route('auctions'),
-            'return_url' => route('auction.create'),
-            'type' => 'account_onboarding',
-        ]);
-
-        return redirect($account_link->url);
-    }
 }
--- app/Payment/StripePayment.php
+++ app/Payment/StripePayment.php
@@ -2,9 +2,37 @@
 
 namespace App\Payment;
 
-class StripePayment
-{
+use App\Models\User;
+use Illuminate\Http\Response;
+use Mauricius\LaravelHtmx\Http\HtmxResponseClientRedirect;
+use Stripe\BaseStripeClient;
+use Stripe\Exception\ApiErrorException;
+use Stripe\StripeClient;
 
+class StripePayment extends PaymentGateway
+{
+    protected BaseStripeClient $stripe;
+    public function __construct()
+    {
+        $this->stripe = new StripeClient(config('stripe.sk'));
+    }
+
+    /**
+     * @throws ApiErrorException
+     */
+    public function createSellerAccount(User $user, string $accountType): Response
+    {
+        if (!is_null($user->stripe_account_id)) {
+            return new HtmxResponseClientRedirect(route('payment.login'));
+        }
+
+        $connect_account = $this->stripe->accounts->create(['type' => 'express']);
+        $user->update(['stripe_account_id' => $connect_account->id]);
+        $user->save();
+
+        //TODO: this is pretty roundabout way to redirect. should just use back()?
+        return new HtmxResponseClientRedirect(back()->getTargetUrl());
+    }
     public function pay($amount)
     {
         //TODO: Implement stripe payment
